1aaa8908f5927a50521c73a3af98e87442196b29
Rawan-Hamza <rawanhamza@hotmail.com>
2023-02-19 23:58:32 +0000

    fix resume link for a better view

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://docs.google.com/document/d/1PLj2m8os8LWsSG2uCR52GiMIjaRbL2Ud/edit?usp=sharing&ouid=109215652535139534596&rtpof=true&sd=true"
+                  href="https://drive.google.com/file/d/1Q9NrcqNqJPuwZz0fRaq74xDME3uVNKi6/view?usp=share_link"
                   target="_blank"
                   rel="noreferrer"
                 >
